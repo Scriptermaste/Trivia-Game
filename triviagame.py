21961e348ea66e2d17b9084204c6ba728280761f
Scriptermaste <murarisbanka@gmail.com>
2025-06-17 21:41:49 -0400

    Trivia Game
    Very bad trivia game to be honest with you.

---
+++ triviagame.py
@@ -0,0 +1,18 @@
+question = (input("What color is the sky? "))
+question1 = (input("Who is the first president of the United States? "))
+question2 = (input("What is the color of my bugatti? "))
+answer = "Blue"
+answer1 = "George Washington"
+answer2 = "Blue"
+if question == answer:
+    print("Yes that is correct!")
+else:    
+    print("No that is incorrect")
+if question1 == answer1:
+    print("Yes that is correct!")
+else:    
+    print("No that is incorrect")
+if question2 == answer2:
+    print("Yes that is correct!")
+else:    
+    print("No that is incorrect")
